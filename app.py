6cd4434a3e2a1937ab1390d0b95573d304fa4886
Reyn Alpha <reynalpha@outlook.com>
2021-06-07 14:53:31 +0000

    Initial work on (working) switching functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 import subprocess
 
@@ -27,20 +27,54 @@ def send_ssh_command(command: list[str]):
 
 
 def start_vpn():
-    """Starts the VPN"""
-    send_ssh_command(["service", config["ovpn_service"], "start"])
+    """Starts the VPN, returns the exit code"""
+    return send_ssh_command(["service", config["ovpn_service"], "start"])
 
 
 def stop_vpn():
-    """Stops the VPN"""
-    send_ssh_command(["service", config["ovpn_service"], "stop"])
+    """Stops the VPN, returns the exit code"""
+    return send_ssh_command(["service", config["ovpn_service"], "stop"])
 
 
 def select_server(address: str):
-    # TODO
-    pass
+    """Selects the specified address as the VPN server, returns the exit code"""
+    return send_ssh_command(["set", f"vpn_client1_addr={address}"])
 
 
 @app.route("/")
 def index():
     return render_template("index.html", hosts=config["ovpn_hosts"])
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    # Get form data
+    data = request.form
+    host = data.get("host")
+    enabled = data.get("enabled") == "On"
+
+    # If an invalid host is submitted
+    if host not in config["ovpn_hosts"]:
+        print(f"Error: {host} is not defined in the configuration file")
+        # TODO: return error
+        return
+
+    # Select server
+    if not select_server(host):
+        print(f"Error: Could not set server to {host}")
+        # TODO: return error
+        return
+
+    # Start or stop VPN depdending on choice
+    if enabled:
+        if not start_vpn():
+            print("Error: Could not start VPN")
+            # TODO: return error
+            return
+    else:
+        if not stop_vpn():
+            print("Error: Could not stop VPN")
+            # TODO: return error
+            return
+
+    return "success"
